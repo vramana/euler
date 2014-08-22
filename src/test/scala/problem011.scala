9ca13131dff323926bb15bb04314a96a657ef914
Venkata Ramana <idlike2dream@gmail.com>
2014-08-23 00:31:17 +0530

    unfinished problem 11

---
+++ src/test/scala/problem011.scala
@@ -0,0 +1,23 @@
+import euler.euler11._
+
+import org.scalatest.FunSuite 
+
+class Euler11 extends FunSuite {
+
+  test("top") {
+     assert(leftProd === 2)
+  }
+
+  test("down") {
+     assert(down === 1)
+  }
+
+  test("diagRight") {
+     assert(diagRight === 1)
+  }
+
+  test("diagLeft") {
+    assert(diagLeft === 1)
+  }
+
+}
